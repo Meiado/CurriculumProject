b984e708cf3438261fa5398927301676122a403a
Meiado <gabrielmeiado@hotmail.com>
2023-06-21 11:56:17 -0300

    Barras de habilidades animadas e alterações no css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,32 @@
-const observer = new IntersectionObserver(entries => {
+const percentObserver = new IntersectionObserver(entries => {
     console.log(entries)
 
     Array.from(entries).forEach(entry => {
-        if(entry.intersectionRatio >= 0.5)
+        if(entry.intersectionRatio >= 1){
+            if(entry.target.classList.contains('hide')){
+                entry.target.classList.remove('hide')
+                entry.target.setAttribute('style', `width: ${Number(entry.target.ariaValueNow)}%; transition: width 1s ease-in-out;`)
+            }
+        }
+    })
+})
+
+Array.from(document.querySelectorAll('.percentual')).forEach(element => {
+    percentObserver.observe(element)
+})
+
+const observer = new IntersectionObserver(entries => {
+   // console.log(entries)
+
+    Array.from(entries).forEach(entry => {
+        if(entry.intersectionRatio >= 0.5){
             entry.target.classList.add('init-hidden-off')
+        }
     })
 },{
     threshold: [0, 0.5, 1]
 })
+
 
 Array.from(document.querySelectorAll('.init-hidden')).forEach(element => {
     observer.observe(element)
